6bafdb305b71d8f1c9360bc70c9b2b3f035a3da1
Sanjay Lowanshi <sanjay.l@simformsolutions.com>
2023-08-03 12:43:04 +0530

    cahllenge 1&2

--- Challenge 01/index.js
+++ Challenge 01/index.js
@@ -2,18 +2,24 @@
 
 // Using the below variables in a template literal make a string 'Today's date is 23rd July 2023, temprature today will be 32 Celcius and weather will be Cloudy'
 
-const day = '23rd July 2023';
-const temprature = '32 Celcius';
-const weather = 'Cloudy';
+const d = '23rd July 2023';
+const t = '32 Celcius';
+const w = 'Cloudy';
 
-console.log('Print the result here');
+console.log(`Today's date is ${d}, temprature today will be ${t} and weather will be ${w}.`);
 
-// Now instead of using static variables use a make a function that accepts day, temprature and weather as parameters and print the same string using a function
+// using static variables use a make a function that accepts day, temprature and weather as parameters and print the same string using a function
 
-function printString() {
-  console.log();
+function printString(d, t, w) {
+  console.log(`Today's date is ${d}, temprature today will be ${t} and weather will be ${w}.`);
 }
 
-printString();
+printString('22 April 1998', '28 Celcius', 'Sunny');
 
-// Bonus challenge: Instead entering the date manually use the JavaScript Date Object (MDN LINK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date) to get the date and pass it to the function.
+// Bonus challenge:
+
+function bonusChallenge() {
+  const todayDate = new Date();
+  console.log(`Today's date is ${todayDate}.`);
+}
+bonusChallenge();
--- Challenge 02/index.js
+++ Challenge 02/index.js
@@ -1,16 +1,12 @@
 // Challenge 02 Shorthand Property Names
 
-// This practicle will not have any outputs just write your answer codes for bonus you can try to use the functions to do something and check if they work properly or not.
-
-// Convert the below code to have shorthand property names
-
 function formatMessage(name, id, avatar) {
   return {
-    name: name,
-    id: id,
-    avatar: avatar,
+    name,
+    id,
+    avatar,
     timestamp: Date.now(),
-    save: function () {
+    save() {
       console.log('Message saved');
     },
   };
